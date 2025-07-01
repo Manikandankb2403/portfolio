b75289c0723d2b7c428bfce2df4aaf6133e1a4ea
ManikandanKB2403 <manikandankb2403@gmail.com>
2025-07-01 21:46:18 +0530

    resume update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = ({ darkMode }) => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 20 }}
-            whileHover={{ scale: 1.1, rotate: 5 }}
+            whileHover={{ scale: 1.1, rotate: 6,boxShadow: "0 0 25px rgba(99,102,241,0.4)" }}
             whileTap={{ scale: 0.9 }}
           >
             <FaArrowUp />
